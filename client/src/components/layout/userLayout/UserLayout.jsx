8c6c8184a34068bad7e62f58c2022e11f5e888c6
Alju Sabu <aljusabu555@gmail.com>
2026-09-19 16:09:56 +0530

    user layout dessign changed

--- client/src/components/layout/userLayout/UserLayout.jsx
+++ client/src/components/layout/userLayout/UserLayout.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import UserMenu from "./UserMenu";
+import UserNavbar from "./UserNavbar";
 
 const UserLayout = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
   return (
-    <>
-      <div className="min-h-screen w-full flex">
-        <UserMenu />
-        <main className="grow p-5">
+    <div className="flex min-h-screen bg-zinc-50">
+      <UserMenu
+        isMobileMenuOpen={isMobileMenuOpen}
+        setIsMobileMenuOpen={setIsMobileMenuOpen}
+      />
+
+      <main className="flex-1 md:ml-64 flex flex-col min-h-screen">
+        <UserNavbar setIsMobileMenuOpen={setIsMobileMenuOpen} />
+
+        <div className="p-6 sm:p-8 max-w-6xl w-full mx-auto flex-1">
           <Outlet />
-        </main>
-      </div>
-    </>
+        </div>
+      </main>
+    </div>
   );
 };
 
